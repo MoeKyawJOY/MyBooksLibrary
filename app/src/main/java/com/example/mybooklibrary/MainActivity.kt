aa7fddc3bbfd42801bb50cbbaa98e48e79b7b4bd
MoeKyawJOY <moekyaw2101@gmail.com>
2021-11-09 14:32:44 +0630

    change logo

--- app/src/main/java/com/example/mybooklibrary/MainActivity.kt
+++ app/src/main/java/com/example/mybooklibrary/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.mybooklibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
